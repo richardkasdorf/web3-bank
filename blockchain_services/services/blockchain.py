9ac0ad8d953a733c50bb1fef088e7480121ebdd6
RichardKasdorf <richardkasdorf1987@gmail.com>
2026-04-29 21:09:40 -0300

    Blockchain transfer route + crud

--- blockchain_services/services/blockchain.py
+++ blockchain_services/services/blockchain.py
@@ -18,7 +18,7 @@ def get_private_key_from_vault():
             path='config'
         )
         secrets = read_response['data']['data']
-        return secrets
+        return secrets['secretkey']
     
     except Exception as e:
         raise Exception(f"❌ Vault access error: {str(e)}")
@@ -28,6 +28,7 @@ def get_private_key_from_vault():
 SEPOLIA_RPC = os.getenv("SEPOLIA_RPC")
 USDC_CONTRACT = os.getenv("USDC_CONTRACT")
 PUBLIC_ADDRESS = os.getenv("PUBLIC_ADDRESS")
+PUBLIC_ADDRESS_TEST = os.getenv("PUBLIC_ADDRESS_TEST")
 
 if not all([SEPOLIA_RPC, USDC_CONTRACT]):
     raise Exception("❌ Ambient variables missing (SEPOLIA_RPC or USDC_CONTRACT)")
@@ -61,40 +62,6 @@ ERC20_ABI = [
 
 usdc_contract = w3.eth.contract(address=USDC_CONTRACT, abi=ERC20_ABI)
 
-# ====================== Services ======================
-
-def transfer_usdc(to_address: str, amount: Decimal) -> str:
-    if not w3.is_address(to_address):
-        raise ValueError(f"Invalid address: {to_address}")
-
-    private_key = get_private_key_from_vault()
-    bank_address = w3.eth.account.from_key(private_key).address
-
-    amount_wei = int(amount * Decimal("1000000"))
-
-    try:
-        nonce = w3.eth.get_transaction_count(bank_address)
-
-        tx = usdc_contract.functions.transfer(to_address, amount_wei).build_transaction({
-            'chainId': 11155111,
-            'gas': 120000,
-            'gasPrice': w3.eth.gas_price,
-            'nonce': nonce,
-        })
-
-        signed_tx = w3.eth.account.sign_transaction(tx, private_key)
-        tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
-        receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-        if receipt['status'] == 0:
-            raise Exception(f"Blockchain transsacion error. Hash: {w3.to_hex(tx_hash)}")
-
-        return w3.to_hex(tx_hash)
-
-    except Exception as e:
-        raise Exception(f"Transfer error: {str(e)}")
-
-
-
 ''' python -c "import blockchain; blockchain.get_bank_balance()" '''
 def get_bank_balance():
     if w3.is_connected():
@@ -120,7 +87,17 @@ def get_eth_balance():
     print(f"Real balance: {balance_eth} ETH")
     return balance_eth
 
-
-
+''' python -c "import blockchain; blockchain.get_bank_balance_test()" '''
+def get_bank_balance_test():
+    if w3.is_connected():
+        print("✅ Online on Sepolia!")
+    else:
+        print("❌ Connection fail.")
+    decimais = usdc_contract.functions.decimals().call()
+    usdc_balance = usdc_contract.functions.balanceOf(PUBLIC_ADDRESS_TEST).call()
+    real_balance = usdc_balance / (10 ** decimais)
+    print(f"Balance: {usdc_balance}")
+    print(f"Decimals: {decimais}")
+    print(f"Real Balance: {real_balance} USDC")
 
 
